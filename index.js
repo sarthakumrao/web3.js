59478fd2c0a16b912a1334e61f19d7370e2ba4e5
sarthak <sarthak.umrao@dltlabs.io>
2019-04-10 04:18:37 -0500

    Use a different name for older web3Use a different name for older web3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var Web3 = require('./lib/web3');
+var Web3_0_20_6 = require('./lib/web3');
 
 // dont override global variable
 if (typeof window !== 'undefined' && typeof window.Web3 === 'undefined') {
-    window.Web3 = Web3;
+    window.Web3 = Web3_0_20_6;
 }
 
-module.exports = Web3;
+module.exports = Web3_0_20_6;
